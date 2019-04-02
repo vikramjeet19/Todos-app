646ec504f789dfc7944bf2f4d5b246185197a83b
vikramjeet singh <singh_vicky83@yahoo.com>
2019-04-02 21:50:54 +0530

    edit modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import List from './List/list';
-import './App.css'
+import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTimesCircle, faEdit } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faTimesCircle,faEdit);
+library.add(faTimesCircle, faEdit);
 export default class App extends Component {
 
   state = {
     term: '',
-    items: []
+    items: [],
+    edit: false
   }
   onChange = (event) => {
     this.setState({ term: event.target.value });
@@ -17,8 +18,19 @@ export default class App extends Component {
 
   onSubmit = (event) => {
     event.preventDefault();
+    if (this.state.edit) {
+      const allItems = this.state.items;
+      allItems[this.state.editItemIndex] = this.state.term
+      this.setState({
+        items: allItems,
+        edit: false,
+        term: ''
+      });
+      return;
+    }
     this.setState({
-      items: [...this.state.items, this.state.term]
+      items: [...this.state.items, this.state.term],
+      term: ''
     });
 
   }
@@ -26,19 +38,17 @@ export default class App extends Component {
     let arr = [];
     arr = [...this.state.items];
     arr.splice(index, 1);
-    this.setState({items:arr})
+    this.setState({ items: arr })
   }
-  editHandler=()=>{
-    let arr = [...this.state.items];
-    arr.find(function(index){
-      return console.log(this.state.items[index])}
-    );
-    
+  editHandler = (index) => {
+
+    this.setState({ edit: true, term: this.state.items[index], editItemIndex: index });
   }
 
   render() {
+
     return (
-      <div style={{textAlign:'center'}}>
+      <div style={{ textAlign: 'center' }}>
         <h1>To-Do List</h1>
         <form className="App" onSubmit={this.onSubmit}>
           <input className='field' value={this.state.term} onChange={this.onChange} />
@@ -46,7 +56,8 @@ export default class App extends Component {
         </form>
         <List items={this.state.items}
           deleteHandler={(index) => this.deleteHandler(index)}
-          editHandler={(index)=>{this.editHandler(index)}} />
+          editHandler={(index) => { this.editHandler(index) }} />
+
       </div>
     );
   }
